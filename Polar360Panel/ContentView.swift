3ea24dcf6c25ec76b0f42ba5d32b0fbd2bd5c349
r-hasegawa <r-hasegawa@mspa.med.osaka-u.ac.jp>
2026-07-24 01:03:04 +0900

    Initial Commit

---
+++ Polar360Panel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Polar360Panel
+//
+//  Created by Ryosuke Hasegawa on 2026/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Polar360Panel/Polar360PanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Polar360PanelApp.swift
+//  Polar360Panel
+//
+//  Created by Ryosuke Hasegawa on 2026/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Polar360PanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
